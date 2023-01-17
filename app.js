7b08f07b95d5b55bb206c9b8c2921c511016354d
paolaguzmanok <paogu20@hotmail.com>
2023-01-17 18:55:27 -0300

    agrega ruta

--- app.js
+++ app.js
@@ -9,20 +9,23 @@ const publicpath = path.join(__dirname, "./public");
 app.use(express.static(publicpath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
+  res.sendFile(path.join(__dirname, "views/users/index.html"));
 });
 
 app.get("/detalle", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/detalle.html"));
+  res.sendFile(path.join(__dirname, "views/products/detalle.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/login.html"));
+  res.sendFile(path.join(__dirname, "views/users/login.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/register.html"));
+  res.sendFile(path.join(__dirname, "views/users/register.html"));
 });
 app.get("/carrito", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/carrito.html"));
+  res.sendFile(path.join(__dirname, "views/users/carrito.html"));
+});
+app.get("/carga", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/users/carga.html"));
 });
