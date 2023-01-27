29e9d4c9f1592c112b4990d08114283af52b76ec
paolaguzmanok <paogu20@hotmail.com>
2023-01-27 16:16:48 -0300

    agrega carpeta data

---
+++ src/data/productos.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const productosFilePath = path.join(
+  __dirname,
+  "../data/productosBaseDatos.json"
+);
+
+const productosFileContent = fs.readFileSync(productosFilePath, "utf-8");
+const productos = JSON.parse(productosFileContent);
+
+module.exports = productos;
