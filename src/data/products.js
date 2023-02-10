75efb8e91e4e5145312f308be40996622cc7bedb
paolaguzmanok <paogu20@hotmail.com>
2023-02-10 18:48:09 -0300

    elimina y crea carpeta

---
+++ src/data/products.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const productsFilePath = path.join(__dirname, "productsBaseDatos.json");
+
+module.exports = {
+  findAll() {
+    const productsFileContent = fs.readFileSync(productsFilePath, "utf-8");
+    const products = JSON.parse(productsFileContent);
+    return products;
+  },
+  saveProduct(product) {
+    const products = this.findAll();
+    products.push(product);
+    const productsFileContent = JSON.stringify(products, null, 4);
+    fs.writeFileSync(productsFilePath, productsFileContent, "utf-8");
+  },
+  findById(id) {
+    return this.findAll().find((producto) => producto.id == id);
+  },
+};
