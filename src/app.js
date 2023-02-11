2a0445a79e578ba7832cfdd5e09a82178077c89a
paolaguzmanok <paogu20@hotmail.com>
2023-02-11 17:08:17 -0300

    agrega comentario al codigo

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ app.set("views", path.join(__dirname, "/views"));
 
 //Ejecutar archivo mainRouter
 app.use(mainRouter);
+//  Error 404
 app.use((req, res, next) => {
   res.status("404").render("error-404");
 });
