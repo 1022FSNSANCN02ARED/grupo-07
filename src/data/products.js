e8c29938bdcf9d34c646d50f3b89bf5c0893aeb5
paolaguzmanok <paogu20@hotmail.com>
2023-02-20 15:48:50 -0300

    agrega funciones para guardado de la edicion

--- src/data/products.js
+++ src/data/products.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const productsFilePath = path.join(__dirname, "productsBaseDatos.json");
 
+// Funcion de listar productos
+
 module.exports = {
   findAll() {
     const productsFileContent = fs.readFileSync(productsFilePath, "utf-8");
@@ -16,6 +18,25 @@ module.exports = {
     fs.writeFileSync(productsFilePath, productsFileContent, "utf-8");
   },
   findById(id) {
-    return this.findAll().find((producto) => producto.id == id);
+    return this.findAll().find((p) => p.id == id);
+  },
+  saveProductEdited(product) {
+    let products = this.findAll();
+    const productid = product.id;
+    for (let i = 0; i < products.length; i++) {
+      const producto = products[i];
+
+      if (producto.id == productid) {
+        products[i] = product;
+      }
+    }
+    const productsFileContent = JSON.stringify(products, null, 4);
+    fs.writeFileSync(productsFilePath, productsFileContent, "utf-8");
+  },
+  deleteProduct(id) {
+    let products = this.findAll();
+    products = products.filter((p) => p.id != id);
+    const productsFileContent = JSON.stringify(products, null, 4);
+    fs.writeFileSync(productsFilePath, productsFileContent, "utf-8");
   },
 };
