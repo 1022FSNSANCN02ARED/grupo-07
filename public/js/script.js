ef53d0cd239c7909085110085531dcc715db96fd
Luciano Voltattorni <luvolta99@gmail.com>
2023-04-10 20:37:59 -0300

    Okay ahora si juro que es el ultimo jajajaja

--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,32 @@ window.onload = function() {
     const botonMenu= document.querySelector(".header__menu");
 const openMenu = document.querySelector(".openMenu");
 const body = document.querySelector('body');
+const paginaBloqueada = false;
+
 //Abre el menú
 botonMenu.addEventListener("click", ()=>{
     openMenu.classList.toggle("active")
     openMenu.classList.toggle("box-shadow")
-    body.classList.add('no-scroll');
+    botonMenu.classList.toggle('active');
+//Bloquear scroll
+ // Si la página no está bloqueada
+ if (!paginaBloqueada) {
+    // Agregar la clase "active" al botón
+    botonMenu.classList.add('active');
+    // Bloquear la página
+    document.documentElement.style.overflow = 'hidden';
+    // Actualizar el estado de la variable
+    paginaBloqueada = true;
+  } else {
+    // Si la página está bloqueada
+    // Eliminar la clase "active" del botón
+    botonMenu.classList.remove('active');
+    // Desbloquear la página
+    document.documentElement.style.overflow = 'auto';
+    // Actualizar el estado de la variable
+    paginaBloqueada = false;
+  }
+
 })
 }
 
