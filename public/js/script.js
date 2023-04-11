987c714e9452296ea56032b40b442c4292b29e76
Luciano Voltattorni <luvolta99@gmail.com>
2023-04-10 21:51:45 -0300

    Okay ultimo ultimisimo jajajaja

--- public/js/script.js
+++ public/js/script.js
@@ -3,20 +3,20 @@ window.onload = function() {
     const botonMenu= document.querySelector(".header__menu");
 const openMenu = document.querySelector(".openMenu");
 const body = document.querySelector('body');
-const paginaBloqueada = false;
+let paginaBloqueada = false;
 
 //Abre el menú
 botonMenu.addEventListener("click", ()=>{
+  console.log("se ejecutó")
     openMenu.classList.toggle("active")
     openMenu.classList.toggle("box-shadow")
-    botonMenu.classList.toggle('active');
 //Bloquear scroll
  // Si la página no está bloqueada
  if (!paginaBloqueada) {
     // Agregar la clase "active" al botón
     botonMenu.classList.add('active');
     // Bloquear la página
-    document.documentElement.style.overflow = 'hidden';
+    body.style.overflow = 'hidden';
     // Actualizar el estado de la variable
     paginaBloqueada = true;
   } else {
@@ -24,11 +24,10 @@ botonMenu.addEventListener("click", ()=>{
     // Eliminar la clase "active" del botón
     botonMenu.classList.remove('active');
     // Desbloquear la página
-    document.documentElement.style.overflow = 'auto';
+    body.style.overflow = 'auto';
     // Actualizar el estado de la variable
     paginaBloqueada = false;
   }
-
 })
 }
 
