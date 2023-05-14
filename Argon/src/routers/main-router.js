b291f613b5bcc7bdbce70e68e48095130a2692e2
Luciano Voltattorni <luvolta99@gmail.com>
2023-05-14 17:16:35 -0300

    fix edit

--- Argon/src/routers/main-router.js
+++ Argon/src/routers/main-router.js
@@ -12,6 +12,7 @@ const usersRouter = require("../routers/users-router");
 router.use("/users", usersRouter);
 
 //404
+router.get("/error-404",(req,res)=>{res.render("error-404")});
 router.get("/*",(req,res)=>{res.redirect("/error-404")});
 
 module.exports = router;
